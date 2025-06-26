b076c76a0787d67334e5af84661bd018452c65c1
SAM <michaelslocalserver@hotmail.com>
2025-06-26 15:46:07 -0500

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,28 @@
+# Global configuration & tweakables
+WIDTH, HEIGHT = 800, 600
+FPS = 60
+
+# Player
+PLAYER_SPEED = 5
+PLAYER_RADIUS = 12
+PLAYER_COLOR = (255, 255, 255)
+
+# Starfield
+NUM_STARS = 150
+STAR_COLOR = (255, 255, 50)
+STAR_MIN_SIZE, STAR_MAX_SIZE = 2, 5
+
+# Planets
+NUM_PLANETS = 6
+PLANET_COLOR = (0, 200, 0)
+PLANET_MIN_SIZE, PLANET_MAX_SIZE = 12, 35
+
+# Events
+NUM_EVENTS = 4
+EVENT_COLOR = (255, 50, 50)
+EVENT_RADIUS = 16
+EVENT_TYPES = ["Alien Encounter", "Black Hole", "Resource Discovery"]
+
+# Difficulty scaling
+LOOPS_BEFORE_HARD = 5
+SPEED_INCREMENT = 0.5
